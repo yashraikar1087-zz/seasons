160579b9dfd0a185d465020706c3fe87b4668eef
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-14 22:41:57 -0700

    add state

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
   }
 
   //requirement
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
     return <div>Latitude: {this.state.lat}</div>;
   }
 }
+
 ReactDOM.render(<App />, document.querySelector("#root"));
