5f2f3b3d3ed506b99df26c7bb73b84bb794ca6fe
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-16 12:04:56 -0700

    add lat and err message with check

--- src/index.js
+++ src/index.js
@@ -4,19 +4,28 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   //requirement
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    console.log(this.state.errorMessage);
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitue: {this.state.lat}</div>;
+    } else {
+      return <div>Loading</div>;
+    }
   }
 }
 
