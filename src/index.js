e80b8c840fa38122eb775c259705e7bb58e8a2e7
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-16 12:30:00 -0700

    add component did mount

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
@@ -16,9 +18,12 @@ class App extends React.Component {
     );
   }
 
+  componentDidUpdate() {
+    console.log("component update");
+  }
+
   //requirement
   render() {
-    console.log(this.state.errorMessage);
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     } else if (this.state.lat && !this.state.errorMessage) {
