5f7b393612b58046842cb4a33a8593e454e3e92b
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-16 19:40:52 -0700

    add spinner and icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
 const seasonConfig = {
@@ -6,8 +7,8 @@ const seasonConfig = {
     iconName: "sun"
   },
   winter: {
-    text: "Burr, it is chilly",
-    iconName: "winter"
+    text: "Burr, it is cold",
+    iconName: "snowflake"
   }
 };
 
@@ -15,7 +16,7 @@ const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
   } else {
-    return lat > 0 ? "winter" : "summner";
+    return lat > 0 ? "winter" : "summer";
   }
 };
 
@@ -24,10 +25,10 @@ const SeasonDisplay = props => {
   const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`massive ${iconName} icon icon-left`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`massive ${iconName} icon icon-right`} />
     </div>
   );
 };
