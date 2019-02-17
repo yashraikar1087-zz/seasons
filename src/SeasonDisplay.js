c58b143396e320ecabbc3f8a9290a0644ed23c59
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-16 19:18:22 -0700

    refactor icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, it is chilly",
+    iconName: "winter"
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -10,10 +21,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text =
-    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+  const { text, iconName } = seasonConfig[season];
 
-  return <div>{text}</div>;
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
